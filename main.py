6135232f3c01dec38ff06989e0e0c46e7653c838
DarienEstebanVesgaCarvajal <182381184+DarienEstebanVesgaCarvajal@users.noreply.github.com>
2024-11-02 16:55:49 -0500

    asterisk-hexagon

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-high = int(input("What's the height?: "))
+side = int(input("What's the side length?: "))
 
-for x in range(1, high + 1):
-    print('·' * x)
+for x in range(2 * side - 1):
+    if x < side:
+        spaces = side - x - 1
+        dots = side + 2 * x
+    else:
+        spaces = x - side + 1
+        dots = side + 2 * (2 * side - x - 2)    
+
+    print(' ' * spaces + '·' * dots)
